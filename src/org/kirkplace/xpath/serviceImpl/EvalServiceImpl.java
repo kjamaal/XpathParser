994dc8a7ca5bd5195a7bced6903616feff0295c7
unknown <Jamaal.Kirkpatrick@RTCITSDX79PX1.int.asurion.com>
2015-08-08 18:54:53 -0500

    more web service implementation

--- src/org/kirkplace/xpath/serviceImpl/EvalServiceImpl.java
+++ src/org/kirkplace/xpath/serviceImpl/EvalServiceImpl.java
@@ -1,12 +1,17 @@
 package org.kirkplace.xpath.serviceImpl;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
 import org.kirkplace.xpath.service.EvalService;
+import org.kirkplace.xpath.util.XmlAdapter;
 
 @Produces("application/json; charset=UTF-8")
 public class EvalServiceImpl extends BaseService {
@@ -18,13 +23,17 @@ public class EvalServiceImpl extends BaseService {
 
 	@Resource
 	private EvalService eval;
+	private XmlAdapter resp;
 	
-	@GET
+	@POST
 	@Path("/evaluateXpathExpression")
-	public Response evaluateXpathExpr(){
+	public Response evaluateXpathExpr(@Context HttpServletRequest request,
+			@Context HttpServletResponse response){
 		
-		
-		return null;
+		/*
+		 * TODO: Extract xpath and document and pass their string representations. Handle exception.
+		 */
+		return Response.ok(resp.nodeListResponse(eval.evaluateXpathExpr("expression", "document"))).build();
 	}
 	
 }
---
+++ src/org/kirkplace/xpath/util/XmlAdapter.java
@@ -0,0 +1,13 @@
+package org.kirkplace.xpath.util;
+
+import org.w3c.dom.NodeList;
+
+public class XmlAdapter {
+	
+	/*
+	 * TODO: wrap NodeList into whatever response object the UI is expecting.
+	 */
+	public Object nodeListResponse(NodeList list){
+		return "";
+	}
+}
