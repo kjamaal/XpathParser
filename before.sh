65bd3c20d98d41697a4607b88d5ed626ab47c818
unknown <kjamaal81@gmail.com>
2016-01-30 17:53:14 -0600

    typographical

--- before.sh
+++ before.sh
@@ -4,5 +4,5 @@ echo "copying new website"
 scp -i /home/tomcat/.aws/firstKeys.pem /opt/bitnami/apps/jenkins/jenkins_home/jobs/farPoint/workspace/target/FarPoint-1.war bitnami@54.172.88.237:/home/bitnami
 echo "done"
 echo "starting tomcat"
-ssh -i /home/tomcat/.aws/firstKeys.pem bitname@54.172.88.237 "sudo su root -c /bin/bash" < startTC.sh
+ssh -i /home/tomcat/.aws/firstKeys.pem bitnami@54.172.88.237 "sudo su root -c /bin/bash" < startTC.sh
 echo "Deployment done"
