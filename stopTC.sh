4f3a96bfcdea120d1f34c264b15c2be13b19d40c
unknown <kjamaal81@gmail.com>
2016-01-30 18:00:31 -0600

    typo

--- stopTC.sh
+++ stopTC.sh
@@ -2,5 +2,5 @@
 echo "stopping tomcat"
 sudo su root -c '/bin/bash /opt/bitnami/apache-tomcat/bin/shutdown.sh'
 echo "removing old web site"
-rm -R /opt/bitnami/apache-tomcat/webapps/FarPoint-0.0.1-SNAPSHOT /opt/bitnami/apache-tomcat/webapps/FarPoint-0.0.1-SNAPSHOT.war
+rm -R /opt/bitnami/apache-tomcat/webapps/FarPoint-1 /opt/bitnami/apache-tomcat/webapps/FarPoint-1.war
 echo "done"
