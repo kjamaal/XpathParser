343d9a07253e0dae39ee8f68a35cc20cb315fe52
unknown <kjamaal81@gmail.com>
2016-02-02 15:18:41 -0600

    demo

---
+++ cibuild.bat
@@ -0,0 +1,1 @@
+powershell -Command "& ./cibuild.ps1"
---
+++ cibuild.ps1
@@ -0,0 +1,7 @@
+﻿$url="http://ec2-54-165-55-56.compute-1.amazonaws.com/jenkins/job/farPoint/build"
+$request = New-Object System.Net.WebClient
+$auth = "Basic " + [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("user"+":"+"SfVzIkyAWnp7"))
+$request.Headers.Add("AUTHORIZATION",$auth)
+$request.Headers.Add("Accept", "application/json")
+$request.Headers.Add("Content-Type","application/x-www-form-urlencoded")
+$request.UploadString($url,"POST","")
